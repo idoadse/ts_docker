abc3396238fd298b562ff6b83efce526bbf4f8c5
ido.senesh <ido.senesh@similarweb.com>
2019-03-08 16:34:58 +0200

    simple express server

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import * as express from 'express';
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+const app = express();
+
+app.get('/', (req: any, res: any) => {
+    res.send('Hello world!\n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
